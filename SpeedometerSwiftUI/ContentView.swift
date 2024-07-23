d9c2f0bda412c3eea2d61355c38ce8ac9e58bc4a
Maryam Kaveh <maryamk61@gmail.com>
2024-07-23 14:47:10 +0330

    Initial Commit

---
+++ SpeedometerSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeedometerSwiftUI
+//
+//  Created by Maryam Kaveh on 5/2/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeedometerSwiftUI/SpeedometerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedometerSwiftUIApp.swift
+//  SpeedometerSwiftUI
+//
+//  Created by Maryam Kaveh on 5/2/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct SpeedometerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
